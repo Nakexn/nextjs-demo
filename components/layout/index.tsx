241fde1aa1110f76ac6f0b1e2c8612755123eec5
那可心 <nakx@raysdata.com>
2022-10-08 16:41:59 +0800

    feat: 页面完整链路

---
+++ components/layout/index.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { INavBarProps, NavBar } from '../navbar';
+import { IFooterProps, Footer } from '../footer';
+import styles from './styles.module.scss';
+
+export interface ILayoutProps {
+  navbarData: INavBarProps;
+  footerData: IFooterProps;
+}
+
+export const Layout: FC<ILayoutProps & { children: JSX.Element }> = ({ navbarData, footerData, children }) => {
+  return (
+    <div className={styles.layout}>
+      <NavBar {...navbarData} />
+      <main className={styles.main}>{children}</main>
+      <Footer {...footerData} />
+    </div>
+  );
+};
