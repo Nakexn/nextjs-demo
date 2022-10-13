62df816022622d8ea4ecd3caaa139af1cff00dec
那可心 <nakx@raysdata.com>
2022-10-14 01:22:37 +0800

    feat: 设置主题

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import { Fragment } from 'react';
 import type { AppProps, AppContext } from 'next/app';
 import App from 'next/app';
@@ -6,40 +5,44 @@ import { ILayoutProps, Layout } from '@/components/layout';
 import Head from 'next/head';
 import axios from 'axios';
 import { LOCALDOMAIN } from '@/utils';
+import { ThemeContextProvider } from '@/stores/theme';
+import './global.scss';
 
 const MyApp = (data: AppProps & ILayoutProps) => {
-  const { Component, pageProps, navbarData, footerData } = data;
-  return (
-    <Fragment>
-      <Head>
-        <title>A Demo for 《深入浅出SSR官网开发指南》</title>
-        <meta
-          name='description'
-          content='A Demo for 《深入浅出SSR官网开发指南》'
-        />
-        <link
-          rel='icon'
-          href='/favicon.ico'
-        />
-      </Head>
-      <Layout
-        navbarData={navbarData}
-        footerData={footerData}
-      >
-        <Component {...pageProps} />
-      </Layout>
-    </Fragment>
-  );
+	const { Component, pageProps, navbarData, footerData } = data;
+	return (
+		<Fragment>
+			<Head>
+				<title>A Demo for 《深入浅出SSR官网开发指南》</title>
+				<meta
+					name='description'
+					content='A Demo for 《深入浅出SSR官网开发指南》'
+				/>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+			</Head>
+			<ThemeContextProvider>
+				<Layout
+					navbarData={navbarData}
+					footerData={footerData}
+				>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeContextProvider>
+		</Fragment>
+	);
 };
 
 MyApp.getInitialProps = async (context: AppContext) => {
-  const pageProps = await App.getInitialProps(context);
-  const { data = {} } = await axios.get(`${LOCALDOMAIN}/api/layout`);
+	const pageProps = await App.getInitialProps(context);
+	const { data = {} } = await axios.get(`${LOCALDOMAIN}/api/layout`);
 
-  return {
-    ...pageProps,
-    ...data,
-  };
+	return {
+		...pageProps,
+		...data,
+	};
 };
 
 export default MyApp;
